4320acc92066d09a7d078c6801c3d31ecd6abfdd
gweaths <grant.w2011@gmail.com>
2022-12-02 21:12:18 +0000

    Day1 - Task 1

---
+++ source/day1/Program.cs
@@ -0,0 +1,11 @@
+﻿//Read input
+var input = File.ReadAllText("input.txt");
+
+//get Top Elf's Calories
+var topElfCalories = input.Split("\n\n", StringSplitOptions.RemoveEmptyEntries)
+    .Select(
+        elf => elf.Split("\n").ToArray()
+        .Select(x => int.Parse(x))
+        .Sum()).Max();
+
+Console.WriteLine(topElfCalories);
