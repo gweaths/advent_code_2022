b6c301cd79d2b5dbec4ea3950ed0ff86c3d11622
gweaths <grant.w2011@gmail.com>
2022-12-02 21:29:25 +0000

    Day1 - Phase 2

--- source/day1/Program.cs
+++ source/day1/Program.cs
@@ -1,11 +1,26 @@
-﻿//Read input
-var input = File.ReadAllText("input.txt");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Read input
+        var input = File.ReadAllText("input.txt");
 
-//get Top Elf's Calories
-var topElfCalories = input.Split("\n\n", StringSplitOptions.RemoveEmptyEntries)
-    .Select(
-        elf => elf.Split("\n").ToArray()
-        .Select(x => int.Parse(x))
-        .Sum()).Max();
+        var elfCalories = GetElfCalories(input);
+        var topElfCalories = elfCalories.OrderByDescending(x => x).Take(3);
 
-Console.WriteLine(topElfCalories);
+        Console.WriteLine($"Top 3 Elf Calories: {topElfCalories.Sum()}");
+        Console.WriteLine($"Highest Elf Calories: {topElfCalories.First()}");
+    }
+
+    public static List<int> GetElfCalories(string input)
+    {
+        return input.Split("\n\n", StringSplitOptions.RemoveEmptyEntries)
+            .Select(
+                elf => elf.Split("\n").ToArray()
+                .Select(x => int.Parse(x))
+                .Sum())
+                .ToList();
+
+
+    }
+}
