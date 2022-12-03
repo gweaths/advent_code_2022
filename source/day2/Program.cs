462f409e137b0d7fd027ab7ff02e7eff79c3764f
gweaths <grant.w2011@gmail.com>
2022-12-03 02:26:52 +0000

    Day 2 - Rock Paper Scissors

--- source/day2/Program.cs
+++ source/day2/Program.cs
@@ -1,19 +1,59 @@
-﻿string[] input = File.ReadAllLines("input.txt");
-// string[] input = File.ReadAllLines("Sample.txt");
-int totalScore = 0;
-
-foreach (string game in input)
+﻿void Part1()
 {
-    Shape[] moves = game.Split().Select(s => CharToShape(s[0])).ToArray();
-    Shape opponent = moves[0];
-    Shape player = moves[1];
-    totalScore += GetShapeValue(opponent) + GetResults(opponent, player);
+    string[] input = File.ReadAllLines("input.txt");
+    // string[] input = File.ReadAllLines("Sample.txt");
+    int totalScore = 0;
+
+    foreach (string game in input)
+    {
+        Shape[] moves = game.Split().Select(s => CharToShape(s[0])).ToArray();
+        Shape opponent = moves[0];
+        Shape player = moves[1];
+        totalScore += GetShapeValue(opponent) + GetResults(opponent, player);
+    }
+
+    Console.WriteLine("Total Score: " + totalScore);
 }
 
-Console.WriteLine("Total Score: " + totalScore);
+void Part2()
+{
+    string[] input = File.ReadAllLines("Input.txt");
+    int totalScore = 0;
+    foreach (string game in input)
+    {
+        string[] predicted = game.Split();
+        Console.WriteLine(predicted[0][0]);
+        Shape opponent = CharToShape(predicted[0][0]);
+        Outcome outcome = CharToOutcome(predicted[1][0]);
 
+        Shape player = outcome switch
+        {
+            Outcome.Win => opponent.LosesTo(),
+            Outcome.Draw => opponent,
+            Outcome.Lose => opponent.Defeats(),
+            _ => throw new Exception("Could not determine outcome")
+        };
+
+        totalScore += GetShapeValue(player);
+        totalScore += GetResults(player, opponent);
+        Console.WriteLine($"Total Score: {totalScore}");
+    }
+}
+
+Part2();
 
 //---- Private Methods -----
+Outcome CharToOutcome(char ch)
+{
+    return ch switch
+    {
+        'X' => Outcome.Lose,
+        'Y' => Outcome.Draw,
+        'Z' => Outcome.Win,
+        _ => throw new Exception("Invalid character")
+    };
+}
+
 Shape CharToShape(char ch)
 {
     return ch switch
@@ -53,4 +93,11 @@ int GetResults(Shape player, Shape opponent)
     {
         return 0;
     }
+}
+
+enum Outcome
+{
+    Win,
+    Draw,
+    Lose
 }
--- source/day2/Shape.cs
+++ source/day2/Shape.cs
@@ -14,5 +14,27 @@ public static class ShapeExtensions
             || player1 == Shape.Paper && player2 == Shape.Rock;
     }
 
+    public static Shape Defeats(this Shape shape)
+    {
+        return shape switch
+        {
+            Shape.Rock => Shape.Scissors,
+            Shape.Paper => Shape.Rock,
+            Shape.Scissors => Shape.Paper,
+            _ => throw new Exception("Invalid shape provided")
+        };
+    }
+
+    public static Shape LosesTo(this Shape shape)
+    {
+        return shape switch
+        {
+            Shape.Rock => Shape.Paper,
+            Shape.Paper => Shape.Scissors,
+            Shape.Scissors => Shape.Rock,
+            _ => throw new Exception("Invalid shape provided")
+        };
+    }
+
     public static bool Draws(this Shape player1, Shape player2) => player1 == player2;
 }
