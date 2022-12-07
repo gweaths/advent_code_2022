be46f20ea862bd0c0b12e1680ea0ccc7e3e81e24
gweaths <grant.w2011@gmail.com>
2022-12-07 00:40:41 +0000

    Day 6

---
+++ source/Day 6 - Marker/Program.cs
@@ -0,0 +1,23 @@
+﻿var input = File.ReadAllText(args[0]);
+
+Console.WriteLine($"Part1: {FindMarker(input, 4)}");
+Console.WriteLine($"Part2: {FindMarker(input, 14)}");
+
+
+int FindMarker(string input, int amount)
+{
+    if (input.Length == 0)
+    {
+        throw new Exception("No input found");
+    }
+
+    for (int i = 0, count = input.Length; i < count; i++)
+    {
+        var group = input[i..(i + amount)];
+        if (group.ToHashSet().Count() == amount)
+        {
+            return i + amount;
+        }
+    }
+    return 0;
+}
